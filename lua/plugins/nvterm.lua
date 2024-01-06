800986534ec7f8e19c73720ebf0ff61e85eeaa2f
RickHPotter <luisfla55@hotmail.com>
2024-01-05 21:16:50 -0300

    fix: <leader>tt for toggle term was not necessary

--- lua/plugins/nvterm.lua
+++ lua/plugins/nvterm.lua
@@ -5,7 +5,7 @@ local M = {
     keys = {
       -- stylua: ignore
       -- `<Esc><Esc>` enter normal mode, then switch windows
-      { "<leader>tt", function() require("nvterm.terminal").toggle("horizontal") end, desc = "toggle term", mode = { "n", "t" } },
+      -- { "<leader>tt", function() require("nvterm.terminal").toggle("horizontal") end, desc = "toggle term", mode = { "n", "t" } },
     },
     config = function()
       require("nvterm").setup({
